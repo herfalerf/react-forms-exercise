2c4ee672e4d33f62ad8475c8293b15bf83595433
Alba Garcia <albagarcia@Albas-MacBook-Air.local>
2021-06-03 17:12:15 -0500

    first commit, basic app setup

---
+++ src/BoxList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Box from "./Box"
+import NewBoxForm from "./NewBoxForm"
+
+const BoxList = () => {
+
+}
+
+export default BoxList;
---
+++ src/NewBoxForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const NewBoxForm = () => {
+
+}
+
+export default NewBoxForm;
