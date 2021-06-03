ea37ae13876abed8efb3fd05631cc863ff175b42
Alba Garcia <albagarcia@Albas-MacBook-Air.local>
2021-06-03 17:52:54 -0500

    added state for color boxes

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Box from "./Box"
 import NewBoxForm from "./NewBoxForm"
 
 const BoxList = () => {
+const [colorBox, setColorBox] = useState([]);
+const addBox = newColorBox => {
+    setColorBox(colorBox => [...colorBox, newColorBox] );
+}
+
+
 return (
     <div>
      <Box color="red" height="50px" width="75px" />
      <Box color="blue" height="50px" width="75px" />
+     { colorBox.map((color, height, width) => {
+         <Box color={color} height={height} width={width} />
+     })}
 
     </div>
 );
