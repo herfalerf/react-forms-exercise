5e2e7d1b1c95a44a9246ed31c88621aad25cbf98
Alba Garcia <albagarcia@Albas-MacBook-Air.local>
2021-06-03 23:24:10 -0500

    added crappy box form

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,8 +1,53 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const NewBoxForm = () => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(`check out state -> ${formData}`);
+  };
 
-}
+  const [formData, setFormData] = useState({
+    color: "",
+    width: "",
+    height: "",
+  });
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((fData) => ({
+      ...fData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="color">Color:</label>
+      <input
+        id="color"
+        name="color"
+        value={formData.color}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="width">Width:</label>
+      <input
+        id="width"
+        name="width"
+        value={formData.width}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="height">Height:</label>
+      <input
+        id="height"
+        name="height"
+        value={formData.height}
+        onChange={handleChange}
+      />
+      <button>Add a new box</button>
+    </form>
+  );
+};
 
 export default NewBoxForm;
