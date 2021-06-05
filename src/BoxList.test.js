1abc6d0c7b66b734902d6e83d4772b0f8da1b9d6
Alba Garcia <albagarcia@Albas-MacBook-Air.local>
2021-06-04 21:21:40 -0500

    all tests passing for boxes

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import BoxList from "./BoxList";
 
 function addBox(boxList, height = "2", width = "2", color = "peachpuff") {
-  const heightInput = boxList.getByLabelText("height");
-  const widthInput = boxList.getByLabelText("width");
-  const backgroundInput = boxList.getByLabelText("color");
+  const heightInput = boxList.getByLabelText("Height:");
+  const widthInput = boxList.getByLabelText("Width:");
+  const backgroundInput = boxList.getByLabelText("Color:");
   fireEvent.change(backgroundInput, { target: { value: color } });
   fireEvent.change(widthInput, { target: { value: width } });
   fireEvent.change(heightInput, { target: { value: height } });
-  const button = boxList.getByText("Add a new box!");
+  const button = boxList.getByText("Add a new box");
   fireEvent.click(button);
 }
 
